22048eab383bc4dae091279789e4cd8c285f326c
yamahiro20639 <144509349+yamahiro20639@users.noreply.github.com>
2023-09-17 14:40:16 +0900

    第5回課題＿プルリクエスト

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<String> name = new ArrayList<>();
+        name.add("田中");
+        name.add("佐々木");
+        name.add("鈴木");
+        name.add("佐藤");
+        ArrayList<String> result = new ArrayList<>();
+
+        for (String person : name) {
+            if (person.contains("木")) {
+                result.add(person);
+            }
+        }
+        System.out.println(result);
+    }
+}
